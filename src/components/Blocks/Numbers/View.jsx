735edb84f68f0731fb63af0d7b8da54fae8afe1e
Tainara Novaes <tainarasn.sba@gmail.com>
2024-05-07 21:02:47 -0300

    feat: init commit and  editi footer

---
+++ src/components/Blocks/Numbers/View.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Numbers = (props) => {
+  return (
+    <Box sx={{ bgcolor: 'red', p: '1vw', width: 0.5, alignSelf: 'center' }}>
+      I am the Block view component!
+    </Box>
+  );
+};
+
+export default Numbers;
---
+++ src/config.js
@@ -0,0 +1,39 @@
+/** Volto Project Configuration
+ * The recommended way for configuring a Volto project is using an add-on.
+ * The project should only be considered as boilerplate, and all the code and
+ * configuration should happen and be placed in add-ons.
+ *
+ * Both configuring directly and placing code in a project is discouraged, and might be
+ * removed at some point from Volto.
+ *
+ * The local project is left for backwards compatibility for existing projects.
+ */
+
+// [Internal] All the imports of modules required for the configuration *must* happen
+// here BEFORE the following line
+import '@plone/volto/config';
+import Numbers from 'components/Blocks/Numbers/View';
+import numberSVG from '@plone/volto/icons/megaphone.svg';
+import sidebar from '@plone/volto/types/reducers/sidebar/sidebar';
+
+export default function applyConfig(config) {
+  config.settings = {
+    ...config.settings,
+    supportedLanguages: ['pt-br'],
+    defaultLanguages: 'pt-br',
+  };
+  config.blocks.blocksConfig.numbers = {
+    id: 'numbers',
+    title: 'numbers',
+    icon: numberSVG,
+    group: 'text',
+    view: Numbers,
+    // edit: Footer,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: false,
+    blockHasOwnFocusManagement: false,
+  };
+
+  return config;
+}
